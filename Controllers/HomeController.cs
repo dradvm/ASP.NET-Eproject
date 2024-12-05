9b6a1b527435b758ab8d5300e8d90a826a326bbf
Ndcas <ndhung9911@gmail.com>
2024-12-05 20:47:19 +0700

    init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using ABCDMall.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ABCDMall.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Khởi tạo DBContext
+        private ABCDMallEntities db = new ABCDMallEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Copy đoạn này để tự Dispose DBContext sau khi action hoàn thành, tránh tràn dữ liệu
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
