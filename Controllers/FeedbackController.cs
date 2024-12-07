ad3d8cebcefcdd123637f5439c159391f2648cac
Ndcas <ndhung9911@gmail.com>
2024-12-07 16:25:23 +0700

    view + reply feedback

---
+++ Controllers/FeedbackController.cs
@@ -0,0 +1,33 @@
+﻿using ABCDMall.Filters;
+using ABCDMall.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ABCDMall.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        private readonly ABCDMallEntities db = new ABCDMallEntities();
+
+        [AdminFilter]
+        public ActionResult Index()
+        {
+            ViewBag.feedback = db.Feedbacks.OrderByDescending(item => item.SendingTime).ToList();
+            return View();
+        }
+
+        public ActionResult SendFeedback()
+        {
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Scripts/Feedback/index.js
@@ -0,0 +1,5 @@
+﻿$(document).ready(() => {
+    $('#feedback').DataTable({
+        ordering: false
+    });
+});
