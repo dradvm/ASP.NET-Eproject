5fd1882da6af213b5729857187b7c5eecc952179
Ndcas <ndhung9911@gmail.com>
2024-12-08 22:11:11 +0700

    movie add, delete, disable, enable

--- Scripts/Movie/index.js
+++ Scripts/Movie/index.js
@@ -3,3 +3,9 @@
         order: [[4, 'desc']]
     });
 });
+
+function deleteMovie(id) {
+    if (confirm('Are you sure to delete the selected movie along with its showtimes?')) {
+        window.location = `/movie/delete?id=${id}`;
+    }
+}
