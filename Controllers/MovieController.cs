9a7fca7f0d8b9f53290368bb1c0b6e2f5793619e
dradvm <duynnvn2511@gmail.com>
2024-12-12 10:59:09 +0700

    update Movie

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -92,7 +92,84 @@ namespace ABCDMall.Controllers
             image.SaveAs(Path.Combine(Server.MapPath("~/Assets/Images/Movie"), movie.Image));
             db.Movies.Add(movie);
             db.SaveChanges();
-            return Redirect("/movie/add?status=2");
+            return Redirect("/movie/index");
+        }
+
+        [AdminFilter]
+        public ActionResult Update(int id, int status = 1)
+        {
+            ViewData["selectedNav"] = "AdminMovies";
+            Movie movie = db.Movies.FirstOrDefault(item => item.ID == id);
+            if (movie == null)
+            {
+                return Redirect("/movie/index");
+            }
+            ViewBag.movie = movie;
+            ViewBag.status = status;
+            ViewBag.genres = db.Genres.ToList();
+            return View();
+        }
+
+        [AdminFilter]
+        [HttpPost]
+        public ActionResult Update(int id, string name, HttpPostedFileBase image, string director, List<int> genres, DateTime releaseDate, int duration, string description)
+        {
+            Movie movie = db.Movies.FirstOrDefault(item => item.ID == id);
+            if (name == null || name.Trim().Length == 0)
+            {
+                return Redirect("/movie/add?status=0");
+            }
+            movie.Name = name.Trim();
+            if (director == null || director.Trim().Length == 0)
+            {
+                return Redirect("/movie/add?status=-2");
+            }
+            movie.Director = director.Trim();
+            if (genres == null || genres.Count == 0)
+            {
+                return Redirect("/movie/add?status=-3");
+            }
+            movie.Genres.Clear();
+            foreach (int genreI in genres)
+            {
+                Genre genre = db.Genres.FirstOrDefault(item => item.ID == genreI);
+                if (genre == null)
+                {
+                    return Redirect("/movie/add?status=-9");
+                }
+                movie.Genres.Add(genre);
+            }
+            if (releaseDate == null)
+            {
+                return Redirect("/movie/add?status=-4");
+            }
+            movie.RealeaseDate = releaseDate;
+            if (duration <= 0)
+            {
+                return Redirect("/movie/add?status=-5");
+            }
+            movie.Duration = duration;
+            if (description == null || description.Trim().Length == 0)
+            {
+                return Redirect("/movie/add?status=-6");
+            }
+            movie.Description = description.Trim();
+            if (image != null && image.ContentLength > 0)
+            {
+                string[] supported = { ".png", ".jpg", ".jpeg", ".svg" };
+                if (!supported.Any(format => string.Equals(format, Path.GetExtension(image.FileName), System.StringComparison.OrdinalIgnoreCase)))
+                {
+                    return Redirect("/movie/add?status=-7");
+                }
+                movie.Image = HashService.GetHash(image.FileName) + DateTime.Now.ToString("yyyyMMddHHmmss") + Path.GetExtension(image.FileName);
+                if (image.ContentLength > 10000000)
+                {
+                    return Redirect("/movie/add?status=-8");
+                }
+                image.SaveAs(Path.Combine(Server.MapPath("~/Assets/Images/Movie"), movie.Image));
+            }
+            db.SaveChanges();
+            return Redirect("/movie/index");
         }
 
         [AdminFilter]
