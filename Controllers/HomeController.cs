8d8c2e96615f59af080c005950bc3fbef4607786
dradvm <duynnvn2511@gmail.com>
2024-12-13 20:39:00 +0700

    book ticket

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,13 @@
 ﻿using ABCDMall.Models;
 using ABCDMall.Services;
 using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Data.Entity;
+using System.Diagnostics;
 using System.Linq;
 using System.Web.Mvc;
+using System.Windows.Documents;
 
 namespace ABCDMall.Controllers
 {
@@ -22,27 +25,72 @@ namespace ABCDMall.Controllers
         public ActionResult Movie(int variance = 0)
         {
             DateTime date = DateTime.Now.Date.AddDays(variance);
-            ViewBag.movies = db.Movies.Where(movie => movie.Showtimes.Any(showtime => DbFunctions.TruncateTime(showtime.StartingTime) == date.Date));
-            ViewData["selectedNav"] = "Movies";
+            ViewBag.movies = db.Movies.Where(movie => movie.Showtimes.Any(showtime => DbFunctions.TruncateTime(showtime.StartingTime) == date.Date)).ToList();
             ViewBag.variance = variance;
             return View();
         }
 
-            
-        //Test thanh toán
-        public ActionResult TestPayment()
+        public ActionResult Seat(int id)
         {
+            ViewBag.showtime = db.Showtimes.FirstOrDefault(showtime => showtime.ID == id);
+            
+            return View();
+        }
+
+
+        //Test thanh toán
+        [HttpPost]
+        public ActionResult Payment(List<int> seats, int total, String name, String email, int showtime)
+        {
+            List<Ticket> tickets = seats.Select(seat => new Ticket
+            {
+                Seat = seat,
+                Showtime = showtime,
+                CustomerName = name,
+                CustomerEmail = email,
+                Price = db.Seats.FirstOrDefault(x => x.ID == seat)?.SeatType1.Price ?? 0,
+                PaymentTime = DateTime.Now
+            }).ToList();
+            db.Tickets.AddRange(tickets);
+            db.SaveChanges();
+
+            Session["CurrentTicketIds"] = tickets.Select(t => t.ID).ToList();
+
             VNPayService vnp = new VNPayService();
             //Truyền số tiền cần thanh toán, nội dung thanh toán, URL trả về
-            return Redirect(vnp.CreateRequestUrl(100000, "Test", ConfigurationManager.AppSettings["weburl"] + "/home/testresult"));
+            return Redirect(vnp.CreateRequestUrl(total, "Test", ConfigurationManager.AppSettings["weburl"] + "/home/result"));
         }
 
         //Test xử lý kết quả thanh toán
-        public ActionResult TestResult()
+        public ActionResult Result()
         {
             VNPayService vnp = new VNPayService();
             //True nếu thanh toán thành công, False nếu ngược lại
-            ViewBag.Status = vnp.ValidateSignature(Request);
+            var check = vnp.ValidateSignature(Request);
+            ViewBag.Status = check;
+            var ids = Session["CurrentTicketIds"] as List<int>;
+
+            if (ids == null || !ids.Any())
+            {
+                // Không có vé nào cần xử lý
+                ViewBag.Status = false;
+                return View();
+            }
+
+            var tickets = db.Tickets.Where(t => ids.Contains(t.ID) && t.PaymentTime == null).ToList();
+
+            if (check)
+            {
+                foreach (var ticket in tickets)
+                {
+                    ticket.PaymentTime = DateTime.Now;
+                }
+            }
+            else
+            {
+                db.Tickets.RemoveRange(tickets);
+            }
+            db.SaveChanges();
             return View();
         }
 
---
+++ Scripts/Home/seat.js
@@ -0,0 +1,59 @@
+﻿
+    var total = 0;
+
+document.getElementById("total-text").innerText = total;
+document.getElementById("total").value = total;
+        const calcTotal = (e, money) => {
+            var seat = e.target.id.replace("seat_check", "") - 0
+    if (!document.getElementById("seat" + seat).checked) {
+        total += money;
+            }
+    else {
+        total -= money
+    }
+    document.getElementById("total-text").innerText = total;
+    document.getElementById("total").value = total;
+        }
+
+        const chooseSweetBox = (e, money) => {
+            var seat1 = e.target.id.replace("seat_check", "") - 0
+    var seat2 = seat1 % 2 == 0 ? seat1 - 1 : seat1 + 1
+    document.getElementById("seat" + seat2).checked = !document.getElementById("seat" + seat1).checked
+    document.getElementById("seat" + seat1).checked = document.getElementById("seat" + seat1).checked
+    if (!document.getElementById("seat" + seat1).checked) {
+        total += money;
+            }
+    else {
+        total -= money
+    }
+    document.getElementById("total").innerText = total;
+        }
+
+const mySubmit = () => {
+    form = document.querySelector("#myForm")
+    if (document.querySelectorAll('input[id^="seat"]:checked').length == 0) {
+        alert("PLease select at least 1 seat!")
+    }
+    else if (document.querySelectorAll('input[id^="seat"]:checked') > 8) {
+        alert("You can select aYou can only select up to 8 seats at a time")
+    }
+    else if (!form.reportValidity()) {
+
+    }
+    else {
+        form.submit()
+    }
+}
+
+    {/*$.ajax({*/}
+    {/*type: 'post',*/}
+    {/*url: `/home/payment`,*/}
+    {/*data: {*/}
+    {/*    id: id*/}
+    {/*},*/}
+    {/*success: function (response) {*/}
+    {/*    if (response == 'OK') {*/}
+    {/*        location.href = '/gallery/index';*/}
+    {/*    }*/}
+    {/*}*/}
+    {/*})*/}
