5306d9b442612c1e2729cab02e410870b8a7ffa3
dradvm <duynnvn2511@gmail.com>
2024-12-17 20:53:42 +0700

    swiper gallery

---
+++ Scripts/Home/home.js
@@ -0,0 +1,12 @@
+﻿const swiper = new Swiper('.swiper', {
+    // Cấu hình Swiper
+    direction: 'horizontal', // Trượt ngang
+    slidesPerView: 4,        // Hiển thị 4 phần tử
+    spaceBetween: 20,        // Khoảng cách giữa các phần tử
+    loop: true,              // Cho phép lặp vô hạn
+
+    autoplay: {
+        delay: 3000, // Thời gian chờ giữa các slide (đơn vị: ms)
+        disableOnInteraction: false, // Không dừng autoplay khi người dùng tương tác
+    },
+});
